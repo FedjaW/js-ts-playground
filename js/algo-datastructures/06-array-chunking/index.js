93bf8d6a66ed86636b2be6eb77ef8b6892705c96
Friedrich Wilms <fwilms@dspace.de>
2023-08-23 14:45:16 +0200

    chunk arrays

--- js/algo-datastructures/06-array-chunking/index.js
+++ js/algo-datastructures/06-array-chunking/index.js
@@ -9,7 +9,30 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+    const finalArray = []
+    let tempArray = []
 
+    for (let index = 0; index < array.length + 1 ; index++) {
+
+        for (let i = 0; i < size; i++) {
+            if (array[i] !== undefined) {
+                tempArray.push(array[i])
+            }
+        }
+
+        for (let i = 0; i < size; i++) {
+            array.shift();
+        }
+
+        finalArray.push(tempArray)
+        tempArray = []
+    }
+
+
+    console.log(finalArray);
 }
+
+chunk([1,2,3,4,5,6,7,8,9,10], 4)
+chunk(["a", "b", "c", "d", "e"], 4)
 
 module.exports = chunk;
