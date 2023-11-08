79e4b0d9104d016bb5bb0d7304b9ddada6c84e8b
Friedrich Wilms <fwilms@dspace.de>
2023-11-08 08:12:05 +0100

    ordner umbennant

---
+++ javascript/algo-datastructures/16-linkedlist/index.js
@@ -0,0 +1,22 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {}
+
+class LinkedList {}
+
+
+const nodeOne = {
+    data: 123
+}
+
+const nodeTwo = {
+    data: 456
+}
+
+nodeOne.next = nodeTwo
+
+console.log(nodeOne);
+
+module.exports = { Node, LinkedList };
