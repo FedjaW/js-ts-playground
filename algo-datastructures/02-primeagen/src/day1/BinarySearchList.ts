76c52e4262c8873de94b9e027de61ccd51bdd1f9
FedjaW <f.wilms89@web.de>
2023-11-12 17:35:51 +0100

    binary serach

--- algo-datastructures/02-primeagen/src/day1/BinarySearchList.ts
+++ algo-datastructures/02-primeagen/src/day1/BinarySearchList.ts
@@ -1,1 +1,22 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+    // 0 ........x......... N
+    // lo + ((hi - lo) / 2)
+    var l = 0;
+    var h = haystack.length;
+    
+    do {
+        var mid = Math.floor(l + ((h - l) / 2));
+        var value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        }
+        if (value > needle) {
+            h = mid;
+        } else {
+            l = mid + 1;
+        }
+    } while (l < h);
+
+    return false;
+}
